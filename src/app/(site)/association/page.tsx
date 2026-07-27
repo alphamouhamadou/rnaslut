765e8726ebd8665ec3d64849a65365f967a4155a
alphamouhamadou <121497396+alphamouhamadou@users.noreply.github.com>
2026-07-27 15:47:04 +0200

    Update page.tsx

--- src/app/(site)/association/page.tsx
+++ src/app/(site)/association/page.tsx
@@ -1,5 +1,4 @@
 import { db } from '@/lib/db';
-import PresentationSection from '@/components/rnaslut/PresentationSection';
 import ResultsSection from '@/components/rnaslut/ResultsSection';
 import PerspectivesSection from '@/components/rnaslut/PerspectivesSection';
 import PartnersSection from '@/components/rnaslut/PartnersSection';
@@ -15,7 +14,6 @@ export default async function AssociationPage() {
 
   return (
     <div className="pt-20">
-      <PresentationSection />
       <ResultsSection />
       <PerspectivesSection perspectives={perspectives} />
       <PartnersSection partners={partners} />
